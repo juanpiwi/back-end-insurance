ff6418e8e7511966b28608b2fe5912dab935a67d
Juampi <ferrarijuanp@gmail.com>
2018-09-19 17:58:04 -0300

    configure unit test.

---
+++ api/services/tests/insurance.spec.js
@@ -0,0 +1,55 @@
+/**
+ * Module dependencies
+ */
+const { expect } = require('chai')
+const nock = require('nock')
+const sinon = require('sinon')
+
+const { mocky } = require('./../../../config')
+const InsuranceService = require('./../insurance')
+
+const mockedLogger = {
+  debug: sinon.stub(),
+  info: sinon.stub()
+}
+const serverResponse = {
+  clients: [
+    {
+      id: '111111',
+      name: 'Britney',
+      email: 'britneyblankenship@quotezart.com',
+      role: 'admin'
+    },
+    {
+      id: '2222222',
+      name: 'Manning',
+      email: 'manningblankenship@quotezart.com',
+      role: 'admin'
+    }
+  ]
+}
+
+describe('Insurance service - List', () => {
+  afterEach(() => {
+    mockedLogger.info.reset()
+    mockedLogger.debug.reset()
+  })
+
+  it('should successfuly', (done) => {
+    nock(mocky.apiUrl)
+      .get('/5808862710000087232b75ac')
+      .reply(200, serverResponse)
+
+    // Call InsuranceService list method
+    InsuranceService.list().then((result) => {
+      // Check response is as expected
+      expect(result).to.contain.all.keys(['clients'])
+      expect(result).to.deep.equal(serverResponse)
+      result.clients.forEach(element => {
+        expect(element).to.contain.all.keys(['id', 'name', 'email', 'role'])
+      })
+      // Next Test!
+      done()
+    })
+  })
+})
